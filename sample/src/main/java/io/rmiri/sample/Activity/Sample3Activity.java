4aba1cb8c44f6cdde4ab08de19bfe4416c1b5598
Rasoul Miri <miri.rasoul@gmail.com>
2017-08-26 23:52:55 +0430

    change name to Skeleton

---
+++ sample/src/main/java/io/rmiri/sample/Activity/Sample3Activity.java
@@ -0,0 +1,66 @@
+package io.rmiri.sample.Activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import io.rmiri.skeleton.Master.SkeletonDetail;
+import io.rmiri.sample.Adapter.AdapterSample3;
+import io.rmiri.sample.Data.DataObject;
+import io.rmiri.sample.Data.GenarateDataFake;
+import io.rmiri.sample.R;
+
+
+public class Sample3Activity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private AdapterSample3 adapterSample3;
+    private ArrayList<DataObject> dataObjects = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_3);
+
+
+        //toolbar
+        ((Toolbar) findViewById(R.id.toolbar)).setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //initial recyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        // initial SkeletonDetail and set in adapter
+        SkeletonDetail skeletonDetail = new SkeletonDetail();
+        skeletonDetail.setSkeletonIsOn(true);
+        adapterSample3 = new AdapterSample3(getApplicationContext(), dataObjects, skeletonDetail);
+
+        //set adapter in recyclerView
+        recyclerView.setAdapter(adapterSample3);
+
+
+        //after 5 second get data fake
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dataObjects = new GenarateDataFake().genarateDataFake();
+                adapterSample3.addMoreDataAndSkeletonFinish(dataObjects);
+            }
+        }, 5000);
+    }
+
+
+}
---
+++ sample/src/main/java/io/rmiri/sample/Adapter/AdapterSample1.java
@@ -0,0 +1,135 @@
+package io.rmiri.sample.Adapter;
+
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageButton;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import io.rmiri.skeleton.Master.SkeletonDetail;
+import io.rmiri.skeleton.SkeletonGroup;
+import io.rmiri.sample.Data.DataObject;
+import io.rmiri.sample.R;
+
+
+public class AdapterSample1 extends RecyclerView.Adapter<AdapterSample1.ViewHolder> {
+
+    private Context context;
+    private ArrayList<DataObject> dataObjects = new ArrayList<>();
+    private SkeletonDetail skeletonDetail = new SkeletonDetail();
+
+    public AdapterSample1(Context context, ArrayList<DataObject> dataObjects, @Nullable SkeletonDetail skeletonDetail) {
+        this.context = context;
+        this.dataObjects = dataObjects;
+        if (skeletonDetail != null) {
+            this.skeletonDetail = skeletonDetail;
+        }
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sample_1, parent, false);
+        return new ViewHolder(view);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CardView cardView;
+        private SkeletonGroup placeHolderGroup;
+        private AppCompatImageView photoACImgV;
+        private TextView newFlagTv;
+        private TextView titleTv;
+        private TextView descriptionTv;
+        private AppCompatImageButton addToParkingImgBtn;
+        private AppCompatImageButton compareImgBtn;
+        private TextView priceTv;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            cardView = (CardView) itemView.findViewById(R.id.cardView);
+            placeHolderGroup = (SkeletonGroup) itemView.findViewById(R.id.placeHolderGroup);
+            photoACImgV = (AppCompatImageView) itemView.findViewById(R.id.photoACImgV);
+            newFlagTv = (TextView) itemView.findViewById(R.id.newFlagTv);
+            titleTv = (TextView) itemView.findViewById(R.id.titleTv);
+            descriptionTv = (TextView) itemView.findViewById(R.id.descriptionTv);
+            addToParkingImgBtn = (AppCompatImageButton) itemView.findViewById(R.id.addToParkingImgBtn);
+            compareImgBtn = (AppCompatImageButton) itemView.findViewById(R.id.compareImgBtn);
+            priceTv = (TextView) itemView.findViewById(R.id.priceTv);
+
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.cardView.setPreventCornerOverlap(false);
+
+//        holder.placeHolderGradientGroup.setPosition(position);//just for debug log
+
+        if (skeletonDetail.isSkeletonIsOn()) {
+            //need show s for 2 cards
+            holder.placeHolderGroup.setAutoPlay(true);
+            return;
+        } else {
+            holder.placeHolderGroup.setShowPlaceHolder(false);
+            holder.placeHolderGroup.finishAnimation();
+        }
+
+        //set data in view
+        final DataObject cardObj = dataObjects.get(position);
+
+        holder.titleTv.setText(cardObj.getTitle());
+        holder.descriptionTv.setText(cardObj.getDescription());
+        holder.priceTv.setText(cardObj.getPrice());
+
+        if (cardObj.isNew()) {
+            holder.newFlagTv.setVisibility(View.VISIBLE);
+        } else {
+            holder.newFlagTv.setVisibility(View.GONE);
+        }
+
+        //set photo by Picasso lib
+        Picasso.with(context).load(cardObj.getPhoto()).into(holder.photoACImgV);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (skeletonDetail.isSkeletonIsOn()) {
+            // show just 2 card item in recyclerView
+            return 2;
+        } else {
+            //normal show card item in recyclerView
+            return dataObjects.size();
+        }
+    }
+
+
+    public void addMoreDataAndSkeletonFinish(ArrayList<DataObject> dataObjects) {
+
+        //add new data to dataObjects
+        this.dataObjects = new ArrayList<>();
+        this.dataObjects.addAll(dataObjects);
+
+        //set false show s
+        skeletonDetail.setSkeletonIsOn(false);
+
+        //update items cardView
+        notifyDataSetChanged();
+    }
+
+
+}
